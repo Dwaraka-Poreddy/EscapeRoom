932ed33f69ecd55b4873250872941143720c3a2c
unknown <f20170855@hyderabad.bits-pilani.ac.in>
2021-01-13 19:33:55 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { Switch, Route } from "react-router-dom";
+import PuzzlePage from "./PuzzlePage";
+import VioletPage from "./VioletPage";
+export default function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={PuzzlePage} />
+        <Route exact path="/violetpage" component={VioletPage} />
+      </Switch>
+    </div>
+  );
+}
